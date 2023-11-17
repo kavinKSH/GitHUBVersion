81cbbaca4bde844eeb4af973e7d0b4333f9ccb85
kavinKSH <kavin14795@gmail.com>
2023-11-17 21:23:34 +0530

    change future 2 and app delegate

--- GitHUBVersion/ViewController.swift
+++ GitHUBVersion/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Future- 1")
+        print("Future- 2")
     }
 
 
